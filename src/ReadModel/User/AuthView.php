7ff98b29afc03edee6a6afe571c8163e90996caf
Julia <julsuniverse@gmail.com>
2020-04-03 16:37:15 +0300

    add custom user provider

---
+++ src/ReadModel/User/AuthView.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ReadModel\User;
+
+class AuthView
+{
+    public string $id;
+    public string $email;
+    public string $password_hash;
+    public string $role;
+}
