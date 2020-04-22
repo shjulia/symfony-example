2762f3e5689c1ede10a39075c066e241e2e90ec3
Julia <julsuniverse@gmail.com>
2020-04-22 18:45:21 +0300

    user role console command

--- src/Model/User/UseCase/Role/Command.php
+++ src/Model/User/UseCase/Role/Command.php
@@ -4,8 +4,24 @@ declare(strict_types=1);
 
 namespace App\Model\User\UseCase\Role;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Command
 {
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
     public string $id;
+
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
     public string $role;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
 }
