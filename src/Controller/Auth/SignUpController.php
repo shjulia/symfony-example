63bb1e66c93afe191726a161021537530adef6e5
Julia <julsuniverse@gmail.com>
2020-04-23 17:40:16 +0300

    autologin after email confirmation

--- src/Controller/Auth/SignUpController.php
+++ src/Controller/Auth/SignUpController.php
@@ -5,19 +5,28 @@ declare(strict_types=1);
 namespace App\Controller\Auth;
 
 use App\Model\User\UseCase\Signup;
+use App\ReadModel\User\UserFetcher;
+use App\Security\LoginFormAuthenticator;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class SignUpController extends AbstractController
 {
     private LoggerInterface $logger;
+    /**
+     * @var UserFetcher
+     */
+    private UserFetcher $users;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(UserFetcher $users, LoggerInterface $logger)
     {
         $this->logger = $logger;
+        $this->users = $users;
     }
 
     /**
@@ -51,22 +60,42 @@ class SignUpController extends AbstractController
 
     /**
      * @Route("/signup/{token}", name="auth.signup.confirm")
+     * @param Request $request
      * @param string $token
      * @param Signup\Confirm\ByToken\Handler $handler
+     * @param UserProviderInterface $userProvider
+     * @param GuardAuthenticatorHandler $guardHandler
+     * @param LoginFormAuthenticator $authenticator
      * @return Response
      */
-    public function confirm(string $token, Signup\Confirm\ByToken\Handler $handler): Response
-    {
+    public function confirm(
+        Request $request,
+        string $token,
+        Signup\Confirm\ByToken\Handler $handler,
+        UserProviderInterface $userProvider,
+        GuardAuthenticatorHandler $guardHandler,
+        LoginFormAuthenticator $authenticator
+    ): Response {
+        if (!$user = $this->users->findBySignUpConfirmToken($token)) {
+            $this->addFlash('error', 'Incorrect or already confirmed token.');
+            return $this->redirectToRoute('auth.signup');
+        }
+
         $command = new Signup\Confirm\ByToken\Command($token);
 
         try {
             $handler->handle($command);
             $this->addFlash('success', 'Email is successfully confirmed');
-            return $this->redirectToRoute('home');
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $userProvider->loadUserByUsername($user->email),
+                $request,
+                $authenticator,
+                'main'
+            );
         } catch (\DomainException $e) {
             $this->logger->error($e->getMessage(), ['exception' => $e]);
             $this->addFlash('error', $e->getMessage());
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('auth.signup');
         }
     }
 }
