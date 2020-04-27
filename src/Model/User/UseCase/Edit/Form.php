7ca6cc5b2c44a76624504b8d49b646cda59b0755
Julia <julsuniverse@gmail.com>
2020-04-27 18:00:10 +0300

    add edit user page

---
+++ src/Model/User/UseCase/Edit/Form.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\Edit;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Form extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, ['label' => 'First Name'])
+            ->add('lastName', TextType::class, ['label' => 'Last Name'])
+            ->add('email', EmailType::class, ['label' => 'Email']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Command::class,
+        ));
+    }
+}
