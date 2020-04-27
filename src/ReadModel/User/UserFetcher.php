909962a621b08049a5b4c4af9ef5a3ab610480f1
Julia <julsuniverse@gmail.com>
2020-04-27 19:25:53 +0300

    add user search and filter

--- src/ReadModel/User/UserFetcher.php
+++ src/ReadModel/User/UserFetcher.php
@@ -6,6 +6,7 @@ namespace App\ReadModel\User;
 
 use App\Model\User\Entity\User\User;
 use App\ReadModel\NotFoundException;
+use App\ReadModel\User\Filter\Filter;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\FetchMode;
 use Doctrine\ORM\EntityManagerInterface;
@@ -181,20 +182,45 @@ class UserFetcher
         return $user;
     }
 
-    public function all(): array
+    /**
+     * @param Filter $filter
+     * @return array[]
+     */
+    public function all(Filter $filter): array
     {
-        $stmt = $this->connection->createQueryBuilder()
+        $qb = $this->connection->createQueryBuilder()
             ->select(
                 'id',
                 'date',
-                'TRIM(CONCAT(name_first, \'  \', name_last)) AS name',
+                'TRIM(CONCAT(name_first, \' \', name_last)) AS name',
                 'email',
                 'role',
                 'status'
             )
             ->from('user_users')
-            ->orderBy('date', 'desc')
-            ->execute();
+            ->orderBy('date', 'desc');
+
+        if ($filter->name) {
+            $qb->andWhere($qb->expr()->like('LOWER(CONCAT(name_first, \' \', name_last))', ':name'));
+            $qb->setParameter(':name', '%' . mb_strtolower($filter->name) . '%');
+        }
+
+        if ($filter->email) {
+            $qb->andWhere($qb->expr()->like('LOWER(email)', ':email'));
+            $qb->setParameter(':email', '%' . mb_strtolower($filter->email) . '%');
+        }
+
+        if ($filter->status) {
+            $qb->andWhere('status = :status');
+            $qb->setParameter(':status', $filter->status);
+        }
+
+        if ($filter->role) {
+            $qb->andWhere('role = :role');
+            $qb->setParameter(':role', $filter->role);
+        }
+
+        $stmt = $qb->execute();
 
         return $stmt->fetchAll(FetchMode::ASSOCIATIVE);
     }
